bdc7e7da09afc7748ea759c1fc9b0602018d41c7
Manish <you@example.com>
2023-07-26 22:17:48 +0530

    Updated code adter many changes. timer input box, blocks websites for the provided time and shows notification when blocking starts and stops

--- background.js
+++ background.js
@@ -1,35 +1,107 @@
 let blockingListener = null;
-
+let isBlocking = false
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === "startBlocking") {
-        startBlocking(message.blockedWebsites);
-    } else if (message.action === "stopBlocking") {
-    
-    }
+  if (message.action === "startBlocking") {
+
+    startTimer(message.timerDuration);
+    startBlocking(message.blockedWebsites);
+    isBlocking = true
+    showNotification("Blocking websites has started!");
+  }
+  else if (message.action === "stopBlocking") {
+    isBlocking = false
+    showNotification("Blocking websites has stopped!");
+    stopBlocking();
+  }
+
 });
 
+
+
 function startBlocking(blockedWebsites) {
-    if (!blockingListener) {
-        blockingListener = chrome.webRequest.onBeforeRequest.addListener(
-            function (details) {
-                const url = details.url.toLowerCase();
-                for (const website of blockedWebsites) {
-                    if (url.startsWith(website.toLowerCase()) || url.includes(website.toLowerCase())) {
-                        console.log(blockedWebsites);
-                        return { redirectUrl: chrome.extension.getURL("blocked.html") };
-                    }
-                }
-            },
-            { urls: ["<all_urls>"] },
-            ["blocking"]
-        );
-    }
+
+  // if (isBlocking)
+  {
+    blockingListener = chrome.webRequest.onBeforeRequest.addListener(
+      function (details) {
+        const url = details.url.toLowerCase();
+        for (const website of blockedWebsites) {
+          if (url.startsWith(website.toLowerCase()) || url.includes(website.toLowerCase())) {
+            console.log(blockedWebsites);
+            return { redirectUrl: chrome.extension.getURL("blocked.html") };
+          }
+        }
+      },
+      { urls: ["<all_urls>"] },
+      ["blocking"]
+    );
+  }
+
+  console.log(blockingListener)
+
 }
 
+
 function stopBlocking() {
-    chrome.runtime.reload()
-    if (blockingListener) {
-        chrome.webRequest.onBeforeRequest.removeListener(blockingListener);
-        blockingListener = null;
-    }
+
+  chrome.storage.local.set({ isBlocked: false });
+  setTimeout(function () {
+    chrome.runtime.reload();
+  }, 500);
+  // chrome.runtime.reload()
+  if (blockingListener) {
+
+    chrome.webRequest.onBeforeRequest.removeListener(blockingListener);
+    blockingListener = null;
+
+  }
 }
+
+
+////////////////
+let startTime = null;
+let duration = 0;
+function startTimer(timerDuration) {
+  startTime = new Date();
+  console.log("Timer started at: " + startTime.toLocaleTimeString());
+  duration = timerDuration
+  console.log("Duration: ", duration)
+
+  setTimeout(stopTimer, duration);
+
+}
+
+
+function stopTimer() {
+  if (!startTime) {
+    console.log("Timer hasn't started yet.");
+    return;
+  }
+
+  let currentTime = new Date();
+  let elapsedTime = Math.round((currentTime - startTime) / 1000); // Convert to seconds
+  console.log("Elapsed time: " + elapsedTime + " seconds");
+  showNotification("Blocking websites has stopped!");
+  stopBlocking();
+
+}
+
+//////////////////////
+
+
+function showNotification(message) {
+  chrome.notifications.create({
+    type: "basic",
+    iconUrl: "icon.png",
+    title: "Notification Title",
+    message: message,
+  });
+
+}
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -2,38 +2,69 @@ const myCheckbox = document.getElementById("myCheckbox");
 const blockdiv = document.getElementById("blockdiv");
 const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
+const displayMessage = document.querySelector("#message h2");
+const timerInput = document.getElementById("timerInput");
+const timerdiv=document.getElementById("timerDiv");
 let timerInterval;
-
-// chrome.webRequest.onBeforeRequest.addListener(
-//     function(details) {
-//       if (details.url.includes("www.google.com")) {
-//         return { redirectUrl: chrome.extension.getURL("blocked.html") };
-//       }
-//     },
-//     { urls: ["<all_urls>"] },
-//     ["blocking"]
-//   );
 
 
 myCheckbox.addEventListener("click", function () {
-    console.log("Checked");
-    if (myCheckbox.checked) {
-        blockdiv.style.display = "block";
-    } else {
-        blockdiv.style.display = "none";
-    }
+  console.log("Checked");
+  if (myCheckbox.checked) {
+    blockdiv.style.display = "block";
+  } else {
+    blockdiv.style.display = "none";
+  }
 });
+
+
+
+
 
 startButton.addEventListener("click", function () {
 
-    const blockedWebsitesText = document.getElementById("blockedWebsites").value;
-    const blockedWebsites = blockedWebsitesText.split(/\n/) ; 
-    chrome.runtime.sendMessage({action: "startBlocking" ,blockedWebsites: blockedWebsites });
-   
+  const timerDuration = parseFloat(timerInput.value).toFixed(1);
+  const timerDurationMs = timerDuration * 60 * 1000;
+  console.log("Duration Min: ", timerDuration)
+  console.log("Duration: ", timerDurationMs)
+  if (isNaN(timerDuration) || timerDuration <= 0) {
+    alert("Invalid timer duration. Please enter a valid positive number.");
+    return;
+  }
+
+  const blockedWebsitesText = document.getElementById("blockedWebsites").value;
+  const blockedWebsites = blockedWebsitesText.split(/\n/);
+  chrome.runtime.sendMessage({ action: "startBlocking", blockedWebsites: blockedWebsites, timerDuration: timerDurationMs });
+  chrome.storage.local.set({ isBlocked: true });
+
 }
 )
 stopButton.addEventListener("click", function () {
-    chrome.runtime.reload()
-    console.log("Stop");
-    chrome.runtime.sendMessage({ action: "stopBlocking" });
+  // chrome.runtime.reload()
+  console.log("Stop");
+  chrome.runtime.sendMessage({ action: "stopBlocking" });
+  chrome.storage.local.set({ isBlocked: false });
+
 });
+
+
+
+
+chrome.storage.local.get(["isBlocked"], function (data) {
+  console.log("Heloooooo");
+  if (data.isBlocked === false) {
+    startButton.style.display = "block";
+    stopButton.style.display = "none";
+    myCheckbox.style.display = "block";
+    timerdiv.style.display="block";
+    displayMessage.textContent = "Do you want to block websites";
+  } else {
+    startButton.style.display = "none";
+    stopButton.style.display = "block";
+    myCheckbox.style.display = "none";
+    displayMessage.textContent = "Work Time" ;
+
+    timerdiv.style.display="none";
+  }
+});
+
