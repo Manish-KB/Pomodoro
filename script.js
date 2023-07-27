ba7eb6cfba923838a183428f45c27964da3d10e5
Manish <you@example.com>
2023-07-27 15:28:06 +0530

    Replaced start and stop buttton with toggle switch, added condition to check for empty website list

--- script.js
+++ script.js
@@ -3,36 +3,46 @@ const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
 const displayMessage = document.querySelector("#message p");
 const timerInput = document.getElementById("timerInput");
-const timerdiv=document.getElementById("timerDiv");
+const timerdiv = document.getElementById("timerDiv");
 let timerInterval;
-const textbox=document.getElementById('blockedWebsites')
+const textbox = document.getElementById('blockedWebsites')
+const switchInput = document.getElementById("flexSwitchCheckDefault");
 
+switchInput.addEventListener("change", function () {
+  const isChecked = switchInput.checked;
+  if (isChecked) {
+    const timerDuration = parseFloat(timerInput.value).toFixed(1);
+    const timerDurationMs = timerDuration * 60 * 1000;
+    console.log("Duration Min: ", timerDuration)
+    console.log("Duration: ", timerDurationMs)
+    if (isNaN(timerDuration) || timerDuration <= 0) {
+      switchInput.checked = false
+      alert("Invalid timer duration. Please enter a valid positive number.");
+      return;
+    }
 
+    const blockedWebsitesText = document.getElementById("blockedWebsites").value;
+    const blockedWebsites = blockedWebsitesText.split(/\n/);
 
-startButton.addEventListener("click", function () {
-
-  const timerDuration = parseFloat(timerInput.value).toFixed(1);
-  const timerDurationMs = timerDuration * 60 * 1000;
-  console.log("Duration Min: ", timerDuration)
-  console.log("Duration: ", timerDurationMs)
-  if (isNaN(timerDuration) || timerDuration <= 0) {
-    alert("Invalid timer duration. Please enter a valid positive number.");
-    return;
+    if (blockedWebsites.length > 0 && blockedWebsites[0].trim() !== "") {
+      chrome.runtime.sendMessage({ action: "startBlocking", blockedWebsites: blockedWebsites, timerDuration: timerDurationMs });
+      chrome.storage.local.set({ isBlocked: true });
+    }
+    else {
+      switchInput.checked = false
+      alert("Please enter the website URLs");
+    }
   }
+  else {
+    console.log("Stop");
+    chrome.runtime.sendMessage({ action: "stopBlocking" });
+    chrome.storage.local.set({ isBlocked: false });
 
-  const blockedWebsitesText = document.getElementById("blockedWebsites").value;
-  const blockedWebsites = blockedWebsitesText.split(/\n/);
-  chrome.runtime.sendMessage({ action: "startBlocking", blockedWebsites: blockedWebsites, timerDuration: timerDurationMs });
-  chrome.storage.local.set({ isBlocked: true });
 
+  }
 }
 )
-stopButton.addEventListener("click", function () {
-  console.log("Stop");
-  chrome.runtime.sendMessage({ action: "stopBlocking" });
-  chrome.storage.local.set({ isBlocked: false });
 
-});
 
 
 
@@ -40,18 +50,17 @@ stopButton.addEventListener("click", function () {
 chrome.storage.local.get(["isBlocked"], function (data) {
   console.log("Heloooooo");
   if (data.isBlocked === false) {
-    startButton.style.display = "block";
-    stopButton.style.display = "none";
 
-    timerdiv.style.display="block";
+    switchInput.checked = false
+    timerdiv.style.display = "block";
     displayMessage.textContent = "Enter websites to be blocked";
-    textbox.style.display="block";
+    textbox.style.display = "block";
   } else {
-    startButton.style.display = "none";
-    stopButton.style.display = "block";
-    displayMessage.textContent = "Work Time" ;
-    textbox.style.display="none";
-    timerdiv.style.display="none";
+
+    switchInput.checked = true
+    displayMessage.textContent = "Work Time";
+    textbox.style.display = "none";
+    timerdiv.style.display = "none";
   }
 });
 
